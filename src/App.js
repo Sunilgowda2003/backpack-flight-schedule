b0684a42c2270879d61f754604cce31279adb4ea
Sunilgowda2003 <suni84643@gmail.com>
2026-08-12 14:18:31 +0530

    Create Backpack Flight Schedule app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import BpkButton from 'bpk-component-button';
+import BpkText from 'bpk-component-text';
+import BpkCalendar, {
+  CALENDAR_SELECTION_TYPE,
+} from 'bpk-component-calendar';
+import format from 'date-fns/format';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const formatDateFull = (date) => format(date, 'EEEE, do MMMM yyyy');
+const formatMonth = (date) => format(date, 'MMMM yyyy');
+
+const daysOfWeek = [
+  {
+    name: 'Sunday',
+    nameAbbr: 'Sun',
+    nameNarrow: 'S',
+    index: 0,
+    isWeekend: true,
+  },
+  {
+    name: 'Monday',
+    nameAbbr: 'Mon',
+    nameNarrow: 'M',
+    index: 1,
+    isWeekend: false,
+  },
+  {
+    name: 'Tuesday',
+    nameAbbr: 'Tue',
+    nameNarrow: 'T',
+    index: 2,
+    isWeekend: false,
+  },
+  {
+    name: 'Wednesday',
+    nameAbbr: 'Wed',
+    nameNarrow: 'W',
+    index: 3,
+    isWeekend: false,
+  },
+  {
+    name: 'Thursday',
+    nameAbbr: 'Thu',
+    nameNarrow: 'T',
+    index: 4,
+    isWeekend: false,
+  },
+  {
+    name: 'Friday',
+    nameAbbr: 'Fri',
+    nameNarrow: 'F',
+    index: 5,
+    isWeekend: false,
+  },
+  {
+    name: 'Saturday',
+    nameAbbr: 'Sat',
+    nameNarrow: 'S',
+    index: 6,
+    isWeekend: true,
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectionConfiguration: {
+        type: CALENDAR_SELECTION_TYPE.single,
+        date: null,
+      },
+    };
+  }
+
+  handleDateSelect = (date) => {
+    this.setState({
+      selectionConfiguration: {
+        type: CALENDAR_SELECTION_TYPE.single,
+        date,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <BpkText tagName="h1" textStyle="xxl">
+            Flight Schedule
+          </BpkText>
+        </header>
+
+        <main className="App-main">
+          <BpkCalendar
+            id="flight-schedule-calendar"
+            onDateSelect={this.handleDateSelect}
+            formatMonth={formatMonth}
+            formatDateFull={formatDateFull}
+            daysOfWeek={daysOfWeek}
+            weekStartsOn={1}
+            changeMonthLabel="Change month"
+            nextMonthLabel="Next month"
+            previousMonthLabel="Previous month"
+            selectionConfiguration={this.state.selectionConfiguration}
+          />
+
+          <BpkButton onClick={() => alert('It works!')}>
+            Continue
+          </BpkButton>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Flight Schedule heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const headingElement = screen.getByText(/Flight Schedule/i);
+
+  expect(headingElement).toBeInTheDocument();
 });
